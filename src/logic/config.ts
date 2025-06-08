3b73e76466126a22026ee4b4dbb3fa7e3aff3997
Elihuso Quigley <elihuso@oneamongus.ca>
2025-06-08 08:58:50 +0800

    [U] Use bucket instead of github raw

--- src/logic/config.ts
+++ src/logic/config.ts
@@ -1,4 +1,4 @@
-export const data_uri = 'https://raw.githubusercontent.com/one-among-us/fell-like-data/refs/heads/master/questions.json';
-export const zh_hans_strings = 'https://raw.githubusercontent.com/one-among-us/fell-like-data/refs/heads/master/strings.zh_hans.json'
-export const zh_hant_strings = 'https://raw.githubusercontent.com/one-among-us/fell-like-data/refs/heads/master/strings.zh_hant.json'
-export const ja_jp_strings = 'https://raw.githubusercontent.com/one-among-us/fell-like-data/refs/heads/master/strings.ja_jp.json'
+export const data_uri = 'https://eu2.contabostorage.com/176b8434127b4d90bfb1360e128e586c:oneamongus/falling-data/questions.json';
+export const zh_hans_strings = 'https://eu2.contabostorage.com/176b8434127b4d90bfb1360e128e586c:oneamongus/falling-data/strings.zh_hans.json'
+export const zh_hant_strings = 'https://eu2.contabostorage.com/176b8434127b4d90bfb1360e128e586c:oneamongus/falling-data/strings.zh_hant.json'
+export const ja_jp_strings = 'https://eu2.contabostorage.com/176b8434127b4d90bfb1360e128e586c:oneamongus/falling-data/strings.ja_jp.json'
